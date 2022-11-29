10707c3f8c6de6f24dde916028c25553882cbd56
Patryk Wojtaszek <patryk.wojtaszek@gmail.com>
2022-11-29 02:50:40 +0100

    ok

--- src/test/java/pl/kurs/test4shapes/controller/ShapeControllerTest.java
+++ src/test/java/pl/kurs/test4shapes/controller/ShapeControllerTest.java
@@ -51,58 +51,58 @@ class ShapeControllerTest {
         this.shapeController = new ShapeController(shapeService, modelMapper);
     }
 
-//    @Test
-//    public void addShape_shouldAddShape() throws Exception {
-//        //given
-//        Square shape = new Square(ShapeType.SQUARE, List.of(5.0));
-//        String json = objectMapper.writeValueAsString(shape);
-//
-//        //when
-//                postman.perform(post("/api/v1/shapes")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json))
-//                .andExpect(status().isCreated());
-//
-//        //then
-//        String response2 = postman.perform(MockMvcRequestBuilders.get("/api/v1/shapes/?id=1"))
-//                .andReturn()
-//                .getResponse()
-//                .getContentAsString();
-//
-//        List<Square> responseObjList = objectMapper.readValue(response2, new TypeReference<List<Square>>(){});
-//        Square responseObj = responseObjList.get(0);
-//
-//        Assertions.assertEquals(shape, responseObj);
-//    }
-//
-//    @Test
-//    public void getFilteredShapes_shouldGetFilteredShapes() throws Exception {
-//        //given
-//        Square shape1 = new Square(ShapeType.SQUARE, List.of(5.0));
-//        Circle shape2 = new Circle(ShapeType.CIRCLE, List.of(2.0));
-//        String json1 = objectMapper.writeValueAsString(shape1);
-//        String json2 = objectMapper.writeValueAsString(shape2);
-//
-//        //when
-//        postman.perform(post("/api/v1/shapes")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json1))
-//                .andExpect(status().isCreated());
-//
-//        postman.perform(post("/api/v1/shapes")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json2))
-//                .andExpect(status().isCreated());
-//
-//        //then
-//        String response = postman.perform(MockMvcRequestBuilders.get("/api/v1/shapes/?id=3"))
-//                .andReturn()
-//                .getResponse()
-//                .getContentAsString();
-//
-//        List<Circle> responseObjList = objectMapper.readValue(response, new TypeReference<List<Circle>>(){});
-//        Circle responseObj = responseObjList.get(0);
-//
-//        Assertions.assertEquals(shape2, responseObj);
-//    }
+    @Test
+    public void addShape_shouldAddShape() throws Exception {
+        //given
+        Square shape = new Square(ShapeType.SQUARE, List.of(5.0));
+        String json = objectMapper.writeValueAsString(shape);
+
+        //when
+                postman.perform(post("/api/v1/shapes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isCreated());
+
+        //then
+        String response2 = postman.perform(MockMvcRequestBuilders.get("/api/v1/shapes/?id=1"))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        List<Square> responseObjList = objectMapper.readValue(response2, new TypeReference<List<Square>>(){});
+        Square responseObj = responseObjList.get(0);
+
+        Assertions.assertEquals(shape, responseObj);
+    }
+
+    @Test
+    public void getFilteredShapes_shouldGetFilteredShapes() throws Exception {
+        //given
+        Square shape1 = new Square(ShapeType.SQUARE, List.of(5.0));
+        Circle shape2 = new Circle(ShapeType.CIRCLE, List.of(2.0));
+        String json1 = objectMapper.writeValueAsString(shape1);
+        String json2 = objectMapper.writeValueAsString(shape2);
+
+        //when
+        postman.perform(post("/api/v1/shapes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json1))
+                .andExpect(status().isCreated());
+
+        postman.perform(post("/api/v1/shapes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json2))
+                .andExpect(status().isCreated());
+
+        //then
+        String response = postman.perform(MockMvcRequestBuilders.get("/api/v1/shapes/?id=3"))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        List<Circle> responseObjList = objectMapper.readValue(response, new TypeReference<List<Circle>>(){});
+        Circle responseObj = responseObjList.get(0);
+
+        Assertions.assertEquals(shape2, responseObj);
+    }
 }
